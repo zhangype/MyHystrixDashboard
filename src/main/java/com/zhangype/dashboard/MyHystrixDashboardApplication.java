a1f8db132ad150b0461f9bc32da8542af0bbf98c
zhangype <1141404898@qq.com>
2018-08-20 20:10:38 +0800

    项目初始化

---
+++ src/main/java/com/zhangype/dashboard/MyHystrixDashboardApplication.java
@@ -0,0 +1,21 @@
+﻿package com.zhangype.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Hystrix 仪表盘启动类
+ *
+ * @author 1141404898@qq.com
+ * @version V1.0.0
+ * @date 2018/8/20
+ */
+@EnableHystrixDashboard
+@SpringBootApplication
+public class MyHystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyHystrixDashboardApplication.class, args);
+    }
+}
